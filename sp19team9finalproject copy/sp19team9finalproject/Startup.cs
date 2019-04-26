0774dd2e838a6f1ff377390c8e70c38584339b50
sdobe <40002743+sdobe@users.noreply.github.com>
2019-04-26 14:41:49 -0500

    update startup 04/26

--- sp19team9finalproject copy/sp19team9finalproject/Startup.cs
+++ sp19team9finalproject copy/sp19team9finalproject/Startup.cs
@@ -1,11 +1,12 @@
-﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using sp19team9finalproject.DAL;
 
 namespace sp19team9finalproject
 {
@@ -15,20 +16,21 @@ namespace sp19team9finalproject
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var connectionString = "Server=tcp:sp19team9finalproject.database.windows.net,1433;Initial Catalog=sp19team9finalproject;Persist Security Info=False;User ID=MISAdmin;Password=Password123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+            services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+     
+            // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
         }
+
     }
 }
+
