2f577ca80dabcc83d1988a8b6fcad9166978d047
sdobe <40002743+sdobe@users.noreply.github.com>
2019-04-26 14:48:44 -0500

    updates 4/26

---
+++ sp19team9finalproject copy/sp19team9finalproject/Migrations/20190426191844_InitialCreate.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace sp19team9finalproject.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Companies",
+                columns: table => new
+                {
+                    CompanyID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    EmailAddress = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Industry = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Location = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Companies", x => x.CompanyID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Majors",
+                columns: table => new
+                {
+                    MajorID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Majors", x => x.MajorID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Positions",
+                columns: table => new
+                {
+                    PositionID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Type = table.Column<string>(nullable: true),
+                    Location = table.Column<string>(nullable: true),
+                    Deadline = table.Column<DateTime>(nullable: false),
+                    CompanyID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Positions", x => x.PositionID);
+                    table.ForeignKey(
+                        name: "FK_Positions_Companies_CompanyID",
+                        column: x => x.CompanyID,
+                        principalTable: "Companies",
+                        principalColumn: "CompanyID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AppUsers",
+                columns: table => new
+                {
+                    AppUserID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    EmailAddress = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    GradDate = table.Column<DateTime>(nullable: false),
+                    PositionType = table.Column<int>(nullable: false),
+                    GPA = table.Column<decimal>(nullable: false),
+                    MajorID = table.Column<int>(nullable: true),
+                    CompanyID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppUsers", x => x.AppUserID);
+                    table.ForeignKey(
+                        name: "FK_AppUsers_Companies_CompanyID",
+                        column: x => x.CompanyID,
+                        principalTable: "Companies",
+                        principalColumn: "CompanyID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_AppUsers_Majors_MajorID",
+                        column: x => x.MajorID,
+                        principalTable: "Majors",
+                        principalColumn: "MajorID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MajorDetails",
+                columns: table => new
+                {
+                    MajorDetailID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    MajorID = table.Column<int>(nullable: true),
+                    PositionID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MajorDetails", x => x.MajorDetailID);
+                    table.ForeignKey(
+                        name: "FK_MajorDetails_Majors_MajorID",
+                        column: x => x.MajorID,
+                        principalTable: "Majors",
+                        principalColumn: "MajorID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_MajorDetails_Positions_PositionID",
+                        column: x => x.PositionID,
+                        principalTable: "Positions",
+                        principalColumn: "PositionID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Applications",
+                columns: table => new
+                {
+                    ApplicationID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Result = table.Column<string>(nullable: true),
+                    AppUserID = table.Column<int>(nullable: true),
+                    PositionID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Applications", x => x.ApplicationID);
+                    table.ForeignKey(
+                        name: "FK_Applications_AppUsers_AppUserID",
+                        column: x => x.AppUserID,
+                        principalTable: "AppUsers",
+                        principalColumn: "AppUserID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Applications_Positions_PositionID",
+                        column: x => x.PositionID,
+                        principalTable: "Positions",
+                        principalColumn: "PositionID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Interviews",
+                columns: table => new
+                {
+                    InterviewID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    DateTime = table.Column<DateTime>(nullable: false),
+                    RoomNumber = table.Column<string>(nullable: true),
+                    InterviewerAppUserID = table.Column<int>(nullable: true),
+                    IntervieweeAppUserID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Interviews", x => x.InterviewID);
+                    table.ForeignKey(
+                        name: "FK_Interviews_AppUsers_IntervieweeAppUserID",
+                        column: x => x.IntervieweeAppUserID,
+                        principalTable: "AppUsers",
+                        principalColumn: "AppUserID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Interviews_AppUsers_InterviewerAppUserID",
+                        column: x => x.InterviewerAppUserID,
+                        principalTable: "AppUsers",
+                        principalColumn: "AppUserID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Applications_AppUserID",
+                table: "Applications",
+                column: "AppUserID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Applications_PositionID",
+                table: "Applications",
+                column: "PositionID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppUsers_CompanyID",
+                table: "AppUsers",
+                column: "CompanyID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppUsers_MajorID",
+                table: "AppUsers",
+                column: "MajorID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Interviews_IntervieweeAppUserID",
+                table: "Interviews",
+                column: "IntervieweeAppUserID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Interviews_InterviewerAppUserID",
+                table: "Interviews",
+                column: "InterviewerAppUserID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MajorDetails_MajorID",
+                table: "MajorDetails",
+                column: "MajorID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MajorDetails_PositionID",
+                table: "MajorDetails",
+                column: "PositionID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Positions_CompanyID",
+                table: "Positions",
+                column: "CompanyID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Applications");
+
+            migrationBuilder.DropTable(
+                name: "Interviews");
+
+            migrationBuilder.DropTable(
+                name: "MajorDetails");
+
+            migrationBuilder.DropTable(
+                name: "AppUsers");
+
+            migrationBuilder.DropTable(
+                name: "Positions");
+
+            migrationBuilder.DropTable(
+                name: "Majors");
+
+            migrationBuilder.DropTable(
+                name: "Companies");
+        }
+    }
+}
