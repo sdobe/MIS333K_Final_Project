66571e5694cdea9e162e0e3d4b5ad62955e355ae
nirrauch <46943098+nirrauch@users.noreply.github.com>
2019-04-28 17:27:54 -0500

    Revert "Model Data Annotations"
    This reverts commit 928ad7d50fd0021ee79e09581f196451dc831ab4.

--- sp19team9finalproject copy/sp19team9finalproject/Models/Interview.cs
+++ sp19team9finalproject copy/sp19team9finalproject/Models/Interview.cs
@@ -11,7 +11,6 @@ namespace sp19team9finalproject.Models
         [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy hh:mm tt}")]
         public DateTime DateTime { get; set; }
 
-
         public String RoomNumber { get; set; }
 
         [InverseProperty("InterviewsGiven")]
@@ -19,7 +18,5 @@ namespace sp19team9finalproject.Models
 
         [InverseProperty("InterviewsSuffered")]
         public AppUser Interviewee { get; set; }
-
-        public Position Position { get; set; }
     }
 }
