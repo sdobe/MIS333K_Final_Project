6ce13cb27771f3619635d07c18773ccddd829f15
nirrauch <46943098+nirrauch@users.noreply.github.com>
2019-04-29 15:54:08 -0500

    Startup and HomeController Changes
    Started editing startup

--- sp19team9finalproject copy/sp19team9finalproject/Startup.cs
+++ sp19team9finalproject copy/sp19team9finalproject/Startup.cs
@@ -19,16 +19,38 @@ namespace sp19team9finalproject
             var connectionString = "Server=tcp:sp19team9finalproject.database.windows.net,1433;Initial Catalog=sp19team9finalproject;Persist Security Info=False;User ID=MISAdmin;Password=Password123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
             services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
             services.AddMvc();
+
+            services.AddIdentity<AppUser, IdentityRole>(opts => {
+                opts.User.RequireUniqueEmail = true;
+                opts.Password.RequiredLength = 6;
+                opts.Password.RequireNonAlphanumeric = false;
+                opts.Password.RequireLowercase = false;
+                opts.Password.RequireUppercase = false;
+                opts.Password.RequireDigit = false;
+            })
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddMvc();
         }
 
-     
-            // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider service)
         {
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseStaticFiles();
-            app.UseMvcWithDefaultRoute();
+            app.UseAuthentication();
+            app.UseMvc(routes => {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Home", action = "Index" });
+            });
+            //TODO: This line of code seeds your Identity tables with the correct roles and an admin user
+            //Uncomment this line after the first time this code runs successfully - you don't need 
+            //to seed repeatedly
+            Seeding.SeedIdentity.AddAdmin(service).Wait();
         }
 
     }
