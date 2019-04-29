f73530162081755f5c8af951d079e981d743818d
nirrauch <46943098+nirrauch@users.noreply.github.com>
2019-04-29 17:27:47 -0500

    Add Identity
    Partial Adjustments made

---
+++ sp19team9finalproject copy/sp19team9finalproject/DAL/AppDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+//TODO: Change this using statement to match your project
+using sp19team9finalproject.Models;
+
+//TODO: Make this namespace match your project
+namespace sp19team9finalproject.DAL
+{
+    //NOTE: This class definition references the user class for this project.  
+    //If your User class is called something other than AppUser, you will need
+    //to change it in the line below
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        //TODO: Add your DbSets here.  You will need one for each model you want to store in the database
+        //IMPORTANT: Do NOT add DbSets for your ViewModel classes - they are not stored in your database
+        public DbSet<Application> Applications { get; set; }
+
+        public DbSet<AppUser> AppUsers { get; set; }
+
+        public DbSet<Company> Companies { get; set; }
+
+        public DbSet<Interview> Interviews { get; set; }
+
+        public DbSet<Major> Majors { get; set; }
+
+        public DbSet<MajorDetail> MajorDetails { get; set; }
+
+        public DbSet<Position> Positions { get; set; }
+        //Also, remember that Identity will add a DbSet for your User class.  It will be called Users.  
+        //If you add another DbSet for users, you WILL get an error
+
+    }
+}
---
+++ sp19team9finalproject copy/sp19team9finalproject/Seeding/SeedIdentity.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+
+//: Change these using statements to match your project
+using sp19team9finalproject.DAL;
+using sp19team9finalproject.Models;
+
+//: Change this namespace to match your project
+namespace sp19team9finalproject.Seeding
+{
+    //add identity data
+    public static class SeedIdentity
+    {
+        public static async Task AddAdmin(IServiceProvider serviceProvider)
+        {
+            AppDbContext _db = serviceProvider.GetRequiredService<AppDbContext>();
+            UserManager<AppUser> _userManager = serviceProvider.GetRequiredService<UserManager<AppUser>>();
+            RoleManager<IdentityRole> _roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            //: Add the needed roles
+            //if role doesn't exist, add it
+            if (await _roleManager.RoleExistsAsync("CSO") == false)
+            {
+                await _roleManager.CreateAsync(new IdentityRole("CSO"));
+            }
+
+            if (await _roleManager.RoleExistsAsync("Student") == false)
+            {
+                await _roleManager.CreateAsync(new IdentityRole("Student"));
+            }
+
+            if (await _roleManager.RoleExistsAsync("Recruiter") == false)
+            {
+                await _roleManager.CreateAsync(new IdentityRole("Recruiter"));
+            }
+
+            //check to see if the manager has been added
+            AppUser manager = _db.Users.FirstOrDefault(u => u.Email == "admin@example.com");
+
+            //if manager hasn't been created, then add them
+           
+            if (manager == null)
+            {
+                manager = new AppUser();
+                manager.UserName = "admin@example.com";
+                manager.Email = "admin@example.com";
+                manager.FirstName = "Admin";
+                manager.LastName = "Admin";
+                
+                //TODO: Add additional fields that you created on the AppUser class
+
+
+                var result = await _userManager.CreateAsync(manager, "Abc123!");
+                if (result.Succeeded == false)
+                {
+                    throw new Exception("This user can't be added - " + result.ToString());
+                }
+                _db.SaveChanges();
+                manager = _db.Users.FirstOrDefault(u => u.UserName == "admin@example.com");
+            }
+
+            //make sure user is in role
+            if (await _userManager.IsInRoleAsync(manager, "Manager") == false)
+            {
+                await _userManager.AddToRoleAsync(manager, "Manager");
+            }
+
+            //save changes
+            _db.SaveChanges();
+        }
+
+    }
+}
