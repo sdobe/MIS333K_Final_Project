2c297d69754d22f3987f2112c7182e493bcdab4f
johansballestar <47162770+johansballestar@users.noreply.github.com>
2019-05-05 01:34:18 -0500

    Interview create controller and view

--- sp19team9finalproject copy/sp19team9finalproject/Controllers/InterviewController.cs
+++ sp19team9finalproject copy/sp19team9finalproject/Controllers/InterviewController.cs
@@ -22,7 +22,7 @@ namespace sp19team9finalproject.Controllers
 
         // GET: Interview
         //To-do: student should only see their own interviews query the db
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
             List<Interview> interviews = _context.Interviews();
 
@@ -61,20 +61,14 @@ namespace sp19team9finalproject.Controllers
         //to-do: need to use route value to get application ID 
         //to-do: 
         // GET: Interview/Create
-        public IActionResult Create()
+        public IActionResult Create(Int32 applicationId)
         {
-            //ask identity who is logged in
-            string id = User.Identity.Name;
-            //get user from db 
-            AppUser user = _context.Users.FirstOrDefault(u => u.UserName == id);
-            //Populate viewbags for position, times, and interviewers(recruiters associated with recruiteruser's ID)
+            Interview iv = new Interview();
+            iv.Interview = _context.Applications.Find(applicationId);
+            ViewBag.AllShowings = GetAllShowings();
+
             //make sure you pass the newly created order detail to the view
-            //Populate viewbags with company's associated positions and interviewers
-            ViewBag.Positions = GetAllPositions();
-
-            ViewBag.Interviewers = GetAllInterviewers();
-
-            return View();
+            return View(rd);
         }
 
         // POST: Interview/Create
@@ -177,24 +171,6 @@ namespace sp19team9finalproject.Controllers
         private bool InterviewExists(int id)
         {
             return _context.Interviews.Any(e => e.InterviewID == id);
-        }
-
-        public SelectList GetAllPositions()
-        {
-            List<Position> AllPositions = _context.Positions.ToList();
-
-            SelectList positions = new SelectList(AllPositions, "PositionID", "Title");
-
-            return positions;
-        }
-
-        public SelectList GetAllInterviewers()
-        {
-            List<AppUser> AllAppUsers = _context.AppUsers.ToList();
-
-            SelectList interviewers = new SelectList(AllAppUsers, "AppUserID", "FirstName");
-
-            return interviewers;
         }
     }
 }
