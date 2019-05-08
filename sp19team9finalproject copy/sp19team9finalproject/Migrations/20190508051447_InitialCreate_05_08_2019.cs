846743e8b1be4af996da45bfc4d5784d622b5638
Sophia Dobe <sophia.dobe@gmail.com>
2019-05-08 00:16:09 -0500

    ran migration

---
+++ sp19team9finalproject copy/sp19team9finalproject/Migrations/20190508051447_InitialCreate_05_08_2019.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace sp19team9finalproject.Migrations
+{
+    public partial class InitialCreate_05_08_2019 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmailAddress",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "AspNetUsers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EmailAddress",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+    }
+}
