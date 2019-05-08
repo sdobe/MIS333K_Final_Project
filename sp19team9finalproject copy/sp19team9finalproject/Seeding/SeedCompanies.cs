bee1289fad200dcd8d2f23da3fa7b561d1eeefb2
nirrauch <46943098+nirrauch@users.noreply.github.com>
2019-05-07 20:02:31 -0500

    SeedCompanies

--- sp19team9finalproject copy/sp19team9finalproject/Seeding/SeedCompanies.cs
+++ sp19team9finalproject copy/sp19team9finalproject/Seeding/SeedCompanies.cs
@@ -2,10 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using sp19team9finalproject.DAL;
+using sp19team9finalproject.Models;
 
 namespace sp19team9finalproject.Seeding
 {
-    public class SeedCompanies
+    public static class SeedCompanies
     {
+        public static void SeedAllCompanies(AppDbContext db)
+        {
+            if (db.Companies.Count() == 13)
+            {
+                NotSupportedException ex = new NotSupportedException("Already 13 Companies");
+
+                throw ex;
+            }
+
+            Int32 intCompaniesAdded = 0;
+
+            try
+            {
+                List<Company> Companies = new List<Company>();
+
+                //Company c1 = new Company()
+                //{
+                //    EmailAddress = "accenture@example.com",
+                //    Description = "Whatever",
+                //    Name = "Accenture",
+                //    Industry = "Consulting, Technology"
+                //};
+                //Companies.Add(c1);
+            };
+        }
     }
 }
