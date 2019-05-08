d496bf357e09b19b5b2b56037290f8003cc6a8ed
nirrauch <46943098+nirrauch@users.noreply.github.com>
2019-05-07 21:59:32 -0500

    SeedMajors File

--- sp19team9finalproject copy/sp19team9finalproject/Seeding/SeedMajors.cs
+++ sp19team9finalproject copy/sp19team9finalproject/Seeding/SeedMajors.cs
@@ -24,14 +24,86 @@ namespace sp19team9finalproject.Seeding
 
             try
             {
-                //Company c1 = new Company()
-                //{
-                //    EmailAddress = "accenture@example.com",
-                //    Description = "Whatever",
-                //    Name = "Accenture",
-                //    Industry = "Consulting, Technology"
-                //};
-                //Companies.Add(c1);
+                Major m1 = new Major()
+                {
+                    Name = "MIS"
+                };
+                Majors.Add(m1);
+
+                Major m2 = new Major()
+                {
+                    Name = "Finance"
+                };
+                Majors.Add(m2);
+
+                Major m3 = new Major()
+                {
+                    Name = "Accounting"
+                };
+                Majors.Add(m3);
+
+                Major m4 = new Major()
+                {
+                    Name = "Business Honors"
+                };
+                Majors.Add(m4);
+
+                Major m5 = new Major()
+                {
+                    Name = "Science and Technology Management"
+                };
+                Majors.Add(m5);
+
+                Major m6 = new Major()
+                {
+                    Name = "Management"
+                };
+                Majors.Add(m6);
+
+                Major m7 = new Major()
+                {
+                    Name = "Marketing"
+                };
+                Majors.Add(m7);
+
+                Major m8 = new Major()
+                {
+                    Name = "Supply Chain Management"
+                };
+                Majors.Add(m8);
+
+                Major m9 = new Major()
+                {
+                    Name = "International Business"
+                };
+                Majors.Add(m9);
+
+                try
+                {
+                    foreach (Major majorToAdd in Majors)
+                    {
+                        string strMajorName = majorToAdd.Name;
+                        Major dbMajor = db.Majors.FirstOrDefault(m => m.Name == majorToAdd.Name);
+
+                        if (dbMajor == null)
+                        {
+                            db.Majors.Add(majorToAdd);
+                            db.SaveChanges();
+                            intMajorsAdded += 1;
+                        }
+                        else
+                        {
+                            dbMajor.Name = majorToAdd.Name;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    String msg = " Repositories added:" + intMajorsAdded + "; ";
+
+                    throw new InvalidOperationException(ex.Message + msg);
+
+                }
             }
 
             catch (Exception e)
