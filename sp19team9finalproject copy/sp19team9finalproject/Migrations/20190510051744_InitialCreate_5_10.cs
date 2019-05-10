725ea65ed09a567ac4330379212b1fb49688aa6f
johansballestar <47162770+johansballestar@users.noreply.github.com>
2019-05-10 00:30:51 -0500

    migration + change date time

---
+++ sp19team9finalproject copy/sp19team9finalproject/Migrations/20190510051744_InitialCreate_5_10.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace sp19team9finalproject.Migrations
+{
+    public partial class InitialCreate_5_10 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
